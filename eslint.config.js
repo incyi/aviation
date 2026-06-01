7a0b4cd88dd67d8e076ba5825f6534d9ef2fb31b
incyi <incyi@users.noreply.github.com>
2026-06-01 22:04:52 +0200

    Add ESlint files

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+
+export default [
+  {
+    files: ["assets/js/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  }
+];
